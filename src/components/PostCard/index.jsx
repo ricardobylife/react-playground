60563189365412f54138ce6b9ca4a307db9b2fb5
Ricardo Ribeiro <ricardo.erik@gmail.com>
2021-03-21 01:36:11 -0300

    Paging and componentizing

---
+++ src/components/PostCard/index.jsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+export const PostCard = ({id, title, cover, body}) => (
+    <div className="post">
+        <img src={cover} alt={title} />
+        <div key={id} className="post-content">
+            <h2 key={id}>{title}</h2>
+            <article>{body}</article>
+        </div>
+    </div>
+);
---
+++ src/components/Posts/index.jsx
@@ -0,0 +1,17 @@
+import './styles.css';
+import { PostCard } from "../PostCard";
+
+export const Posts = ({ posts }) => (
+    <div className="posts">
+        {posts.map( post => (
+          <PostCard
+            key={post.id}
+            title={post.title}
+            cover={post.cover}
+            body={post.body}
+            id={post.id}
+          />
+        )
+        )}
+    </div>
+)
